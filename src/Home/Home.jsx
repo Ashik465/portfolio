f73bf3bc58308854c79565e7a11d411b4308f92e
Ashik465 <helloashikul@gmail.com>
2023-07-02 16:12:47 +0600

    home done

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,108 @@
+import { HiArrowDown,  } from "react-icons/hi2";
+import image from "../assets/me/me.png";
+import Typewriter from "typewriter-effect";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+import SphereEffect from "../Sphere/Sphere";
+import resume from '../assets/resume/Resume-of-Ashikul-Islam.pdf'
+
 
 
 const Home = () => {
-    return (
-        <div>
-            home 
+
+
+ 
+  return (
+    <>
+      <section className="mt-5 lg:mt-20 container mx-auto" id="header">
+        <div className="grid grid-cols-1 md:grid-cols-2 w-10/12 mx-auto justify-center items-center">
+          <div className="justify-self-center">
+            <div className="text-left mb-3">
+              <p className="md:text-4xl text-gray-500">Hello, I am 👋</p>
+              <h1 className="md:text-4xl font-bold text-[#FFC107]">
+                Md.Ashikul Islam
+              </h1>
+            </div>
+            <div className="text-left text-red-300 underline underline-offset-8 decoration-[#c9f8e9] font-light md:text-3xl lg:mb-10">
+              <Typewriter
+                options={{
+                  strings: [
+                    "Web Developer",
+                    "MERN Stack Developer",
+                    "Front-End Developer",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                  delay: 50,
+                }}
+              />
+            </div>
+            <p className="text-lg text-gray-500">
+              {" "}
+              From Dhaka, Bangladesh. I specialize in React, Node.js, MongoDB,
+              and Express. 🚀 Whether it is building responsive web applications
+              or creating interactive interfaces, I thrive on turning ideas into
+              elegant code. 🌟 Let us collaborate and bring your digital dreams
+              to life!
+            </p>
+            <div className="flex items-center mt-2">
+              <div className="grid grid-flow-col gap-4">
+                <FaFacebook
+                  onClick={() =>
+                    window.open("https://www.facebook.com/Ashik9305/", "_blank")
+                  }
+                  className="fill-[#FFC107] btn-circle btn-outline p-2 border-0 hover:border-[#FFC107] border-dashed cursor-pointer hover:bg-transparent hover:border-2 active:animate-ping"
+                ></FaFacebook>
+              </div>
+              <div className="grid grid-flow-col gap-4">
+                <FaLinkedin
+                  onClick={() =>
+                    window.open(
+                      "https://www.linkedin.com/in/md-ashikul-islam-07/",
+                      "_blank"
+                    )
+                  }
+                  className="fill-[#FFC107] btn-circle btn-outline p-2 border-0 hover:border-[#FFC107] border-dashed cursor-pointer hover:bg-transparent hover:border-2 active:animate-ping"
+                ></FaLinkedin>
+              </div>
+              <div className="grid grid-flow-col gap-4">
+                <FaGithub
+                  onClick={() =>
+                    window.open("https://github.com/Ashik465", "_blank")
+                  }
+                  className="fill-[#FFC107] btn-circle btn-outline p-2 border-0 hover:border-[#FFC107] border-dashed cursor-pointer hover:bg-transparent hover:border-2 active:animate-ping"
+                ></FaGithub>
+              </div>
+            </div>
+            
+            {/* download */}
+            <a
+              href={resume}
+              download="Resume-of-Ashikul-Islam"
+              target="_blank"
+              rel="noreferrer"
+              className="mt-4 btn btn-outline border-2 hover:bg-[#FFC107] hover:border-[#FFC107] text-red-500"
+            >
+              Download Resume
+              <HiArrowDown className="ml-2"></HiArrowDown>
+            </a>
+            {/*  */}
+          </div>
+          <div className="justify-self-center mask mask-circle">
+            <img
+              src={image}
+              alt="Person Image"
+              className="relative z-10 h-[450px] pt-4"
+            />
+            <div className="absolute top-64 lg:top-0 lg:bottom-0">
+              <SphereEffect />
+            </div>
+           
+          </div>
         </div>
-    );
+     
+      </section>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/Sphere/Sphere.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/no-unknown-property */
+import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+
+const SphereEffect = () => {
+    return (
+        <div className="mt-44 w-96 h-[450px] lg:h-full">
+            <Canvas>
+                <Suspense fallback={null}>
+                    <OrbitControls enableZoom={false} />
+                    <ambientLight intensity={1} />
+                    <directionalLight position={[3, 2, 1]} />
+                    <Sphere args={[1, 100, 100]} scale={2.5}>
+                        <MeshDistortMaterial
+                            color="#FFC107"
+                            attach="material"
+                            distort={0.3}
+                            speed={2}
+                        />
+                    </Sphere>
+                </Suspense>
+            </Canvas>
+        </div>
+    );
+};
+
+export default SphereEffect;
