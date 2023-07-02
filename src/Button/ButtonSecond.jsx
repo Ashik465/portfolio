ef4ed94075a274fb1e41f244ff542beb4c1b65a1
Ashik465 <helloashikul@gmail.com>
2023-07-02 17:46:25 +0600

    project 1 done

---
+++ src/Button/ButtonSecond.jsx
@@ -0,0 +1,13 @@
+
+import { FaGithub } from 'react-icons/fa';
+import './ButtonSecond.css'
+
+const ButtonSecond = () => {
+    return (
+        <button className="button-57 text-yellow-300" role="button"><FaGithub />
+       
+        </button>
+    );
+};
+
+export default ButtonSecond;
--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,10 +1,17 @@
-
+import Showcase1 from "./Showcase1";
+// import Showcase2 from "./Showcase2";
+// import Showcase3 from "./Showcase3";
+// import Showcase4 from "./Showcase4";
 
 const Projects = () => {
     return (
-        <div>
-           projects 
-        </div>
+        <section id="projects" className='w-10/12 mx-auto'>
+      <h1 className='text-center mt-20 font-bold text-xl lg:text-4xl text-[#FFC107]'>Projects Showcase</h1>
+      <Showcase1 />
+      {/* <Showcase2 />
+      <Showcase3 />
+      <Showcase4 /> */}
+    </section>
     );
 };
 
