67981154e0ab3071e58b35976c6d98604c16652c
Ashik465 <helloashikul@gmail.com>
2023-07-02 20:40:22 +0600

    footer and contact me done

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,9 +1,61 @@
-
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
+import Swal from 'sweetalert2';
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_lf836k7', 'template_5c37u19', form.current, 'IszzOlD30LT67n5qH')
+          .then((result) => {
+              console.log(result.text);
+              // success message 
+              Swal.fire({
+                icon: 'success',
+                title: 'Your message has been sent successfully',
+                showConfirmButton: false,
+                timer: 2000
+                })
+                form.current.reset();
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
     return (
-        <div>
-           contact  
+        <div id="contact" className=' container mx-auto my-10'>
+            <div className='text-center font-bold'>
+                <h1 className='text-[#FFC107] text-3xl'>Contact me </h1>  
+            </div>
+           
+           {/* form */}
+           {/* <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="from_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form> */}
+     <form ref={form} onSubmit={sendEmail} className="max-w-md mx-auto bg-white p-6 rounded-md shadow-lg">
+      <div className="mb-4">
+        <label htmlFor="from_name" className="block text-gray-700 font-semibold mb-2">Name</label>
+        <input type="text" name="from_name" id="from_name" className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring focus:border-blue-500" />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="user_email" className="block text-gray-700 font-semibold mb-2">Email</label>
+        <input type="email" name="user_email" id="user_email" className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring focus:border-blue-500" />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="message" className="block text-gray-700 font-semibold mb-2">Message</label>
+        <textarea name="message" id="message" rows="5" className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring focus:border-blue-500"></textarea>
+      </div>
+      <button type="submit" className="bg-[#FFC107] hover:bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-500">
+        Send
+      </button>
+    </form>
+
         </div>
     );
 };
--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,9 +1,47 @@
-
+import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HashLink as Link } from 'react-router-hash-link';
 
 const Footer = () => {
+
     return (
         <div>
-            footer
+            <footer className="footer footer-center p-10  text-base-content rounded">
+            <div className="flex items-center mt-2">
+              <div className="grid grid-flow-col gap-4">
+                <FaFacebook
+                  onClick={() =>
+                    window.open("https://www.facebook.com/Ashik9305/", "_blank")
+                  }
+                  className="fill-[#FFC107] btn-circle btn-outline p-2 border-0 hover:border-[#FFC107] border-dashed cursor-pointer hover:bg-transparent hover:border-2 active:animate-ping"
+                ></FaFacebook>
+              </div>
+              <div className="grid grid-flow-col gap-4">
+                <FaLinkedin
+                  onClick={() =>
+                    window.open(
+                      "https://www.linkedin.com/in/md-ashikul-islam-07/",
+                      "_blank"
+                    )
+                  }
+                  className="fill-[#FFC107] btn-circle btn-outline p-2 border-0 hover:border-[#FFC107] border-dashed cursor-pointer hover:bg-transparent hover:border-2 active:animate-ping"
+                ></FaLinkedin>
+              </div>
+              <div className="grid grid-flow-col gap-4">
+                <FaGithub
+                  onClick={() =>
+                    window.open("https://github.com/Ashik465", "_blank")
+                  }
+                  className="fill-[#FFC107] btn-circle btn-outline p-2 border-0 hover:border-[#FFC107] border-dashed cursor-pointer hover:bg-transparent hover:border-2 active:animate-ping"
+                ></FaGithub>
+              </div>
+            </div>
+                <div className="grid grid-flow-col gap-4 list-none">
+                    <li><Link smooth to="/#header" className='focus:text-[#18C08B] hover:underline'>Home</Link></li>
+                    <li><Link smooth to="/#about" className='focus:text-[#18C08B] hover:underline'>About</Link></li>
+                    <li><Link smooth to="/#projects" className='focus:text-[#18C08B] hover:underline'>Projects</Link></li>
+                    <li><Link smooth to="/#contact" className='focus:text-[#18C08B] hover:underline'>Contact Me</Link></li>
+                </div>
+            </footer>
         </div>
     );
 };
